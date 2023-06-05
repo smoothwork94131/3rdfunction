72dad9e4dfb2bb92f2f9197898ac30648f11c73a
smoothwork94131 <developeralex1994131@gmail.com>
2023-06-05 17:22:35 +0200

    fixed db configuration

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
+    protected $connection = "product";
     protected $table = "categories";
     protected $fillable = ['name', 'slug', 'photo', 'is_featured', 'image', 'skip_license', 'cod', 'order'];
     public $timestamps = false;
